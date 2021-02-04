9e28deb6b909077559fc7c806fd11d2914f1453b
Jake Gealer <jake@gealer.email>
2021-02-04 13:47:30 +0000

    hmmm

--- Dockerfile.php
+++ Dockerfile.php
@@ -5,4 +5,4 @@ RUN composer install
 
 FROM php:8.0-fpm
 RUN apt-get update && apt-get install -y libcurl4-openssl-dev libpq-dev && docker-php-ext-install curl pgsql
-COPY --from=0 /var/app /var/app
+COPY --from=0 /var/app/ /var/app/
